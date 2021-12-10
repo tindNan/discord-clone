90bd115316e9b8e63c07abc333e3fca2cb38baca
Shadrack Lilan <shadrack.lilan@paygoenergy.co>
2021-12-10 11:22:52 +0300

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bg-gray-700 text-white min-h-screen flex flex-col items-center justify-center">
+    <div class="max-w-lg">
+      <div class="flex hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+        <img class="w-10 h-10 rounded-full mr-4" src="../assets/squidward.png">
+        <div>
+          <p class="flex items-baseline">
+            <span class="text-green-500 mr-2 text-sm">Shadrack Lilan</span>
+            <span class="text-xs text-gray-500">14/06/1996</span>
+          </p>
+          <p class="text-gray-300">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Vestibulum a enim sed lectus aliquet iaculis.
+            Sed hendrerit, diam a dictum eleifend, velit eros varius tortor, at hendrerit nulla augue vitae enim.
+            Praesent id sollicitudin diam. Nunc ligula urna, sollicitudin ac enim a, condimentum varius purus.
+            Etiam vitae dignissim mi. Duis vitae odio sed odio feugiat condimentum nec ut mi.
+            Nulla hendrerit mattis vehicula. Curabitur a nulla ut risus rhoncus malesuada.
+          </p>
+        </div>
+      </div>
+      <div className="mt-1 hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+        <p class="text-gray-300 pl-14">
+          Suspendisse sodales sem bibendum augue imperdiet vestibulum.
+          In hac habitasse platea dictumst. Integer hendrerit luctus tincidunt.
+          Integer quis suscipit magna. In leo leo, bibendum sed facilisis eu, bibendum blandit purus.
+        </p>
+      </div>
+      <div className="mt-1 hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+        <p class="text-gray-300 pl-14">
+          In justo magna, mattis ut vulputate pulvinar, feugiat quis dolor.
+          Sed in nisi ut justo pulvinar tempus non at lectus.
+          Donec mi arcu, euismod ac facilisis tincidunt, porta ut sapien.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
